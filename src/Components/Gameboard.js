0ded93924821cde25120f38c21e84bc644b57522
Colin Varney <cvcolinv@gmail.com>
2021-04-24 16:31:54 -0400

    contanerized functions to try and isolate problems. Value updates not being applied correctly.  SetSquareValue function needs addressed

--- src/Components/Gameboard.js
+++ src/Components/Gameboard.js
@@ -9,22 +9,64 @@ class Gameboard extends Component {
       isPlayerOneTurn: false,
       playerOneSign: "X",
       playerTwoSign: "O",
-        
+      currentSign: "X",
+      board: {
+        ID0: "",
+        ID1: null,
+        ID2: null,
+        ID3: null,
+        ID4: null, 
+        ID5: null, 
+        ID6: null, 
+        ID7: null, 
+        ID8: null,       
+      }
+      
     };
   }
     
-    handleChange = (e) => {
-        e.preventDefault();
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-    
-  handleClick = (e)  => {
+  handleChange = (e) => {
+      e.preventDefault();
+      this.setState({
+          [e.target.name]: e.target.value
+      })
+  }
+  setSquareValue = (e) => {
     this.setState({
-      isPlayerOneTurn: !this.state.isPlayerOneTurn,
+      board: {
+        [e.target.id]: this.state.currentSign.value
+      }
     })
-    console.log(` The ID is: ${e.target.id} and the status of player turn is: ${this.state.isPlayerOneTurn}`)
+    
+    console.log(`Square Value Assigned! ${e.target.id} was clicked. ${this.state.currentSign} should be applied. IS it?? ${this.state.board[e.target.id]}`)
+  }
+  //alternate player sign
+  setCurrentSign = () => {
+    if (!this.state.isPlayerOneTurn) {
+      this.setState({
+        currentSign: this.state.playerTwoSign
+      })
+    } else {
+      this.setState({
+        currentSign: this.state.playerOneSign
+      })
+    }  
+  }
+    
+  handleClick = (e) => {
+    this.setCurrentSign();
+    this.setSquareValue(e);
+    // this.setState({
+    //   isPlayerOneTurn: !this.state.isPlayerOneTurn,
+    //   board: {
+    //     [e.target]: this.state.currentSign
+    //   }
+    // })
+    console.log(` The ID is: ${e.target.id} 
+                and the status of player turn is:
+                ${this.state.isPlayerOneTurn} 
+                This square has value: ${this.state.board[e.target.id].value}
+                and the current sign is: ${this.state.currentSign}`)
   }
   render() {
 
@@ -51,8 +93,8 @@ class Gameboard extends Component {
           
 
           <div className="grid" onClick={this.handleClick}>
-            <button className="square" id="0">
-              Space
+            <button className="square" id="ID0" value={this.state.board.ID0}>
+              {this.state.board.ID0}
             </button>
             <button className="square" id="1">
               Space
